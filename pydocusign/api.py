5758feea7a35871f40b8f1da5514e6b9d8b16854
Sam Kuehn <samkuehn@gmail.com>
2016-02-23 12:32:42 -0700

    import datetab

--- pydocusign/api.py
+++ pydocusign/api.py
@@ -30,6 +30,7 @@ from pydocusign.models import Recipient  # NoQA
 from pydocusign.models import Signer  # NoQA
 from pydocusign.models import Role  # NoQA
 from pydocusign.models import SignHereTab  # NoQA
+from pydocusign.models import DateTab  # NoQA
 from pydocusign.models import ApproveTab  # NoQA
 from pydocusign.models import Tab  # NoQA
 from pydocusign.parser import DocuSignCallbackParser  # NoQA
